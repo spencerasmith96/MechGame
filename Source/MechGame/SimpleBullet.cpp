39b15ff049be12f9569dd124813b64cb910c0005
Spencer <spencerasmith96@gmail.com>
2020-06-18 19:47:39 -0400

    change-collision_to_box fix-bullet orientation

--- Source/MechGame/SimpleBullet.cpp
+++ Source/MechGame/SimpleBullet.cpp
@@ -3,37 +3,55 @@
 
 #include "SimpleBullet.h"
 #include "Components/CapsuleComponent.h"
+#include "Components/BoxComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Uobject/ConstructorHelpers.h"
 
 // Sets default values
 ASimpleBullet::ASimpleBullet()
 {
- 	//Use a sphere as simple collision representation
-	CollisionComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collision"));
+ 	//Use a box as simple collision representation
+	CollisionComp = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collision"));
 	CollisionComp->BodyInstance.SetCollisionProfileName("ProjectileKinetic");
 
 	// Players can't walk on it
 	CollisionComp->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
 	CollisionComp->CanCharacterStepUpOn = ECB_No;
 
-	// Set as root component
+	// Set a dummy root component
 	RootComponent = CollisionComp;
 
 	//Mesh visible to player
 	VisibleMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisibleBulletMesh"));
-	VisibleMesh->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	VisibleMesh->AttachToComponent(CollisionComp, FAttachmentTransformRules::KeepRelativeTransform);
 
 	// Use a ProjectileMovementComponent to govern this projectile's movement
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
 	ProjectileMovement->UpdatedComponent = CollisionComp;
-	ProjectileMovement->InitialSpeed = 500.f;
+	ProjectileMovement->InitialSpeed = 3000.f;
 	ProjectileMovement->MaxSpeed = 3000.f;
 	ProjectileMovement->bRotationFollowsVelocity = true;
-	ProjectileMovement->bShouldBounce = false;
+	ProjectileMovement->bShouldBounce = true;
+
+	// If found, uses bullet mesh by default
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> BulletMeshAsset(TEXT("StaticMesh'/Game/CustomContent/Projectiles/BulletMesh.BulletMesh'"));
+	if (BulletMeshAsset.Succeeded())
+	{
+		VisibleMesh->SetStaticMesh(BulletMeshAsset.Object);
+		VisibleMesh->AddLocalRotation(FRotator(270.f, 0.f, 0.f));
+		
+		//Adjusts collision box to fit
+		FVector BulletMinBound, BulletMaxBound;
+		VisibleMesh->GetLocalBounds(BulletMinBound, BulletMaxBound);
+		FVector NewBoxDimensions = BulletMaxBound.ComponentMax(BulletMinBound.GetAbs());
+		NewBoxDimensions.Set(NewBoxDimensions.Z, NewBoxDimensions.Y, NewBoxDimensions.X);
+		
+		CollisionComp->SetBoxExtent(NewBoxDimensions);
+	}
 
 	// Die after 3 seconds by default
 	//UPROPERTY(EditAnywhere)
-	InitialLifeSpan = 3.0f;
+	InitialLifeSpan = 4.f;
 }
 
 void ASimpleBullet::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
--- Source/MechGame/SimpleBullet.h
+++ Source/MechGame/SimpleBullet.h
@@ -13,7 +13,7 @@ class MECHGAME_API ASimpleBullet : public AActor
 
 	/** Sphere collision component */
 	UPROPERTY(EditDefaultsOnly, Category = "ProjectileKinetic")
-	class UCapsuleComponent* CollisionComp;
+	class UBoxComponent* CollisionComp;
 
 	/** Visible mesh of bullet */
 	UPROPERTY(EditAnywhere)
@@ -32,7 +32,7 @@ public:
 	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 	/** Returns CollisionComp subobject **/
-	FORCEINLINE class UCapsuleComponent* GetCollisionComp() const { return CollisionComp; }
+	FORCEINLINE class UBoxComponent* GetCollisionComp() const { return CollisionComp; }
 
 	/** Returns ProjectileMovement subobject **/
 	FORCEINLINE class UProjectileMovementComponent* GetProjectileMovement() const { return ProjectileMovement; }
